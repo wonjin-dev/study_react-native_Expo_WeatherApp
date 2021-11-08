cb775798c966c1940740d5e6fd9f9f692f39368d
WJ951009 <fsdjin159@gmail.com>
2021-11-09 00:16:00 +0900

    flex layout

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, World !</Text>
-      <StatusBar style="light" />
-    </View>
+    <View style={styles.container}></View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: 'white',
-    fontSize: 22
+    flex: 1
   }
-});
+})
