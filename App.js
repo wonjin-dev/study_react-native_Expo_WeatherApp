f2795175bcbb53ac23e843da22e43199a7b72c31
WJ951009 <fsdjin159@gmail.com>
2021-11-09 01:03:34 +0900

    Example application.

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  weather: {
+    flex: 3
+  },
+  day: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 50
+  },
+  cityName: {
+    color: 'white',
+    fontSize: 38,
+    fontWeight: '500'
+  },
+  temp: {
+    color: 'orange',
+    fontSize: 158
   }
-})
+});
--- readme.md
+++ readme.md
@@ -1,12 +1,16 @@
-- ## \<View>
+## \<View>
 > - \<div>과 같이 `컨테이너`이다. <br>
 > - 기본적으로 flex container이며 web과 다르게 기본 방향이 column이다. <br>
-> - 모체 style에 {flex:1}을 할당하고 자식들의 {flex: x}를 할당함으로서 비율로 레이아웃 스타일링이 가능하다.
+> - `모체` style에 `{flex:1}`을 할당하고 `자식`들의 `{flex: x}`를 할당함으로서 `비율`로 `레이아웃 스타일`링이 가능하다.
 <br>
 
-- ## \<Text> 
+## \<Text> 
 > 리액트 네이티브에서 `모든 텍스트`는 \<Text>로 감싸져야 한다. (HTML 태그 사용 불가)
 
-- ## StyleSheet
-> styled-components처럼 사용할 수 있다. (css 객체를 만들 수 있음), StyleSheet가 없어도 css객체를 만들 수 있으나 자동 완성이 되지 않는다.
-> <br><br>ex )<br>```const styles= StyleSheet.create({...})```
+## \<StyleSheet>
+> `css 객체`를 만든다. (StyleSheet가 없어도 css객체를 만들 수 있으나 자동 완성이 되지 않는다.)
+```
+ex)
+
+const styles= StyleSheet.create({...})
+```
